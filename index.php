74b8536e567df03ce19b941989d6718cad0bc3e5
ershov-ilya <ershov.ilya@gmail.com>
2015-02-13 09:17:47 +0300

    php парсинг MIGX

--- index.php
+++ index.php
@@ -10,17 +10,33 @@
  */
 
 defined('DEBUG') or define('DEBUG', true);
-header('Content-Type: application/json; charset=utf-8');
-//header('Content-Type: text/plain; charset=utf-8');
+//header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: text/plain; charset=utf-8');
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 define('MODX_API_MODE', true);
 require('../../index.php');
 
-$id=0;
+$id=550;
 if(isset($_REQUEST['id'])) $id=preg_replace('/[^0-9]/','',$_REQUEST['id']);
-print $id."\n";
+if(DEBUG) print '$id='.$id."\n";
+
+/* @var modX $modx */
+/* @var modResource $resource */
+$resource = $modx->getObject('modResource', $id);
+$migx = $resource->getTVValue(54);
+$data = json_decode($migx);
+if(DEBUG) print_r($data);
+
+$response_arr = array();
+$i=0;
+foreach($data as $el)
+{
+    $response_arr[$i] = $el->link;
+    $i++;
+}
+if(DEBUG) print_r($response_arr);
 
 /*
 {
