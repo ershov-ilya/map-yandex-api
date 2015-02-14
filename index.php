3ea52937600dbe4b641547ff406b5409623c26c8
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-02-14 12:47:26 +0300

    Формирования правильного json'а

--- index.php
+++ index.php
@@ -19,16 +19,16 @@ function replace_quotes($str){
     return $res;
 }
 
-function recurse($resource, $limit=0, $level=0){
+function recurse($resource, $limit=-1, $level=0){
     $data = array();
-    if($limit==0 || $level<$limit) {
+    if($limit<0 || $level<$limit) {
         $children = $resource->getMany('Children');
         foreach ($children as $child) {
             $subdata = recurse($child, $limit, $level + 1);
             if (!empty($subdata)) $data = array_merge($data, $subdata);
         }
     }
-    $migx = $resource->getTVValue(54);
+    $migx = $resource->getTVValue(55);
     $data = array_merge(json_decode($migx), $data);
     return $data;
 }
@@ -49,49 +49,58 @@ if(DEBUG) print '$id='.$id."\n";
 /* @var modX $modx */
 /* @var modResource $resource */
 $resource = $modx->getObject('modResource', $id);
-$migx = $resource->getTVValue(55);
-$data = json_decode($migx);
-if(DEBUG) print_r($data);
-exit(0);
+//$migx = $resource->getTVValue(55);
+//$data = json_decode($migx);
 
-$response_arr = array();
-$i=0;
+$data=recurse($resource);
+//if(DEBUG) print_r($data);
+
+//$response_arr = array();
+//$i=0;
+//foreach($data as $el)
+//{
+//    $response_arr[$i] = array();
+//    $value = $el->address;
+//
+//    $index='';
+//    if(!empty($el->link)) $index.='<a href="'.$el->link.'">';
+//    $index.=replace_quotes($el->name);
+//    if(!empty($el->link)) $index.='</a>';
+//
+//    $response_arr[$i]['index'] = $index;
+//    $response_arr[$i]['value'] = $value;
+////    $el->link;
+//    $i++;
+//}
+//if(DEBUG) print_r($response_arr);
+
+//exit(0);
+
+$i=1;
+$size=sizeof($data);
+$response_json = "[\n";
 foreach($data as $el)
 {
-    $response_arr[$i] = array();
-    $value = $el->address;
-
-    $index='';
-    if(!empty($el->link)) $index.='<a href="'.$el->link.'">';
-    $index.=replace_quotes($el->name);
-    if(!empty($el->link)) $index.='</a>';
-
-    $response_arr[$i]['index'] = $index;
-    $response_arr[$i]['value'] = $value;
-//    $el->link;
+    $response_json .= "{ ";
+    $j=1;
+    $j_size=sizeof((array)$el);
+    foreach($el as $key => $val){
+        $response_json .= "\"$key\" : \"$val\"";
+        if($j<$j_size) $response_json .= ", ";
+        $j++;
+    }
+    $response_json .= " }";
+    if($i<$size) $response_json .= ",";
+    $response_json .= "\n";
     $i++;
 }
-if(DEBUG) print_r($response_arr);
-
-$i=0;
-$size=sizeof($response_arr);
-$response_json = "{\n";
-foreach($response_arr as $el)
-{
-    $i++;
-    $response_json .= '"'.escape_quotes($el['index']).'"';
-    $response_json .= ' : ';
-    $response_json .= '"'.escape_quotes($el['value']).'"';
-    if($i<$size) $response_json .= ",\n";
-}
-$response_json .= "\n}\n\n";
+$response_json .= "]\n\n";
 print $response_json;
-
 /*
 {
 "<a href =\"portfolio/industry/construction-material-production/volsk-cement/\">Реконструкция и модернизация цементного завода ОАО &quot;Вольскцемент&quot;</a>" : "Саратовская область, г. Вольск, ул. Цементников, д. 1",
 "<a href =\"portfolio/commercial-estate/hotel-complexes/volgskaya-rivera/\">Гостиница &quot;Волжская Ривьера&quot;</a>" : "г. Углич, Успенская площадь, д. 8.",
 "<a href =\"portfolio/commercial-estate/hotel-complexes/hampton-by-hilton-volgograd/\">Гостиница HAMPTON BY HILTON VOLGOGRAD</a>" : "г. Волгоград , Центральный р-он, ул. им. Рокоссовского, вл. 51"
-}*/
-
+}
+*/
 
